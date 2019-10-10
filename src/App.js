213fcc5f00b5854d42fd7744156b75ba8af8f843
samuel-lawrence <samuel.lawrence@onthebeach.co.uk>
2019-10-10 14:58:35 +0100

    recipe button loads new recipe

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Recipe from "./Components/Recipe";
 import IngredientsInput from "./Components/IngredientsInput";
 import IngredientsList from "./Components/IngredientsList";
+import RecipeButton from "./Components/RecipeButton";
 
 class App extends PureComponent {
   state = {
     baseIngredients: ["olive oil", "salt", "pepper", "onions"],
-    ingredients: []
+    ingredients: [],
+    recipesShown: 0
   };
 
   submitIngredient = event => {
@@ -20,16 +22,25 @@ class App extends PureComponent {
     }
   };
 
+  showNewRecipe = () => {
+    this.setState(prevState => {
+      return { recipesShown: prevState.recipesShown + 1 };
+    });
+  };
+
   render = () => {
     return (
       <div>
         <h1>Recipe generator</h1>
         <IngredientsList ingredients={this.state.ingredients} />
-        {/* <Recipe
-          ingredients={this.state.ingredients}
-          baseIngredients={this.state.baseIngredients}
-        /> */}
+        {this.state.recipesShown > 0 && (
+          <Recipe
+            ingredients={this.state.ingredients}
+            baseIngredients={this.state.baseIngredients}
+          />
+        )}
         <IngredientsInput submit={this.submitIngredient} />
+        <RecipeButton recipeButtonFn={this.showNewRecipe} />
       </div>
     );
   };
