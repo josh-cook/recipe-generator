f51bf5802416d5bdec8e82e6ff54d49a9be6e00d
samuel-lawrence <samuel.lawrence@onthebeach.co.uk>
2019-10-10 11:38:27 +0100

    Set up Recipe component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import "./App.css";
+import Recipe from "./Components/Recipe";
 
-function App() {
-  return <h1></h1>;
+class App extends PureComponent {
+  state = {
+    baseIngredients: ["olive oil", "salt", "pepper", "onions"],
+    ingredients: [
+      "chicken",
+      "potatoes",
+      "rice",
+      "lamb",
+      "brocolli",
+      "turmeric",
+      "cumin",
+      "coriander"
+    ]
+  };
+
+  render = () => {
+    return (
+      <div>
+        <h1>Recipe generator</h1>
+        <Recipe />
+      </div>
+    );
+  };
 }
 
 export default App;
---
+++ src/Components/Recipe.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Recipe extends Component {
+  state = {};
+
+  render() {
+    return <p>Step1: Wash your hands...</p>;
+  }
+}
+
+export default Recipe;
